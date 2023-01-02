83130befb4b216020ab498e6a09cf32715d03d11
Kratos-750 <www.mightymanme@gmail.com>
2023-01-02 21:50:54 +0530

    minor change 3

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ for (const element of qaArray) {
         let ar = q.childNodes[1];
         if (a.style.display != `block`) {
             a.style.display = `block`
-            q.setAttribute(`class`, `question_open`)
             ar.style.transform = `rotate(180deg)`
             
         } else {
             a.style.display = `none`
             ar.style.transform = `rotate(360deg)`
-            q.removeAttribute(`class`, `question_open`)
-            q.setAttribute(`class`, `question`)
         }
+
+        q.classList.toggle(`question_open`)
+        q.classList.toggle(`question`)
     }
 }
